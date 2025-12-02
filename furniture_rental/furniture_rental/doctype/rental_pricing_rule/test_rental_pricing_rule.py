acc0e84471e74dba41ca4d51b735f4d6788f3091
Maneshnarayanan <maneshk27@gmail.com>
2025-12-02 15:00:55 +0530

    additional doctypes added

---
+++ furniture_rental/furniture_rental/doctype/rental_pricing_rule/test_rental_pricing_rule.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2025, maneshk27@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase
+
+
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record dependencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+
+class IntegrationTestRentalPricingRule(IntegrationTestCase):
+	"""
+	Integration tests for RentalPricingRule.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
